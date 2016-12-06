cdcc4bc3ca91de201d9642cb3ab9d41a11bb0b00
George Seeger <georgeseeger@hotmail.com>
2016-12-06 14:58:23 +0000

    Airport taxiing and test refactor

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -5,17 +5,39 @@ describe("Airport", function() {
   beforeEach(function () {
     airport = new Airport;
     plane = jasmine.createSpyObj('plane',
-      ['land']
+      ['land', 'takeOff']
     )
   });
 
-  it("should accept new airplanes", function(){
-    airport.land(plane)
-    expect(plane.land).toHaveBeenCalled();
+  describe("landing", function() {
+
+    beforeEach(function(){
+      airport.land(plane);
+    });
+
+    it("should accept new airplanes", function(){
+      expect(plane.land).toHaveBeenCalled();
+    });
+
+    it("should store airplanes", function(){
+      expect(airport.planes).toEqual([plane]);
+    });
   });
 
-  it("should store airplanes", function(){
-    airport.land(plane);
-    expect(airport.planes).toEqual([plane]);
+  describe("taking off", function(){
+
+    beforeEach(function(){
+      airport.land(plane);
+      airport.taxi(plane);
+    });
+
+    it("should taxi an airplane", function(){
+      expect(plane.takeOff).toHaveBeenCalled();
+    });
+
+    it("should no longer be  in the airport", function(){
+      expect(airport.planes).toEqual( [] );
+    });
   });
+
 });
--- src/Airport.js
+++ src/Airport.js
@@ -6,3 +6,8 @@ Airport.prototype.land = function(plane){
   plane.land();
   return this.planes.push( plane );
 }
+
+Airport.prototype.taxi = function(plane){
+  plane.takeOff();
+  return this.planes.pop(plane);
+}
